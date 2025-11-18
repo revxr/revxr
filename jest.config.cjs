82f260e47d9514202228635adc0362cb19da1117
revxr <278787873+revxr@users.noreply.github.com>
2025-11-18 14:31:04 +0000

    add the jest config

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};

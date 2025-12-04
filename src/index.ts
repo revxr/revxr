20d6338bcba24546dd703cbdf65d2ba03808d770
revxr <278787873+revxr@users.noreply.github.com>
2025-12-04 18:30:47 +0000

    expose a Report type from the package entry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { analyzeToken } from "./scanner/core.js";
-export type { RevivalReport, AxisScores } from "./scanner/report.js";
-export { computeAxes } from "./scanner/axes.js";
+export type { RevivalReport, AxisScores, AxisName } from "./scanner/report.js";
+export { computeAxes, AXIS_WEIGHTS } from "./scanner/axes.js";
+export { buildRevivalTx } from "./reviver/builder.js";
+export { gini } from "./common/gini.js";
